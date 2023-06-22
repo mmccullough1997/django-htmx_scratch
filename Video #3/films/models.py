a959f87bab43dfd2021b8bfae6a744104d860dea
mmccullough1997 <mmccullough@oningroup.com>
2023-06-22 16:13:00 -0500

    commitin

--- Video #3/films/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    pass
-
-class Film(models.Model):
-    name = models.CharField(max_length=128)
-    users = models.ManyToManyField(User, related_name='films')
